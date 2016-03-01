cc69d19030c553b15498afb3f4cca56317c9ec75
apxoht <ivaylo.kenov@gmail.com>
2016-03-01 22:56:02 +0200

    Added should have memory cache

---
+++ src/MyTested.Mvc/Builders/Actions/ShouldHave/ShouldHaveMemoryCache.cs
@@ -0,0 +1,29 @@
+﻿namespace MyTested.Mvc.Builders.Actions.ShouldHave
+{
+    using Contracts.And;
+    using Contracts.Data;
+    using Data;
+    using System;
+    using Utilities.Validators;
+
+    /// <summary>
+    /// Class containing methods for testing memory cache.
+    /// </summary>
+    /// <typeparam name="TActionResult">Result from invoked action in ASP.NET MVC controller.</typeparam>
+    public partial class ShouldHaveTestBuilder<TActionResult>
+    {
+        public IAndTestBuilder<TActionResult> MemoryCache(int? withNumberOfEntries = null)
+        {
+            return this.NewAndTestBuilder();
+        }
+
+        public IAndTestBuilder<TActionResult> MemoryCache(Action<IMemoryCacheTestBuilder> memoryCacheTestBuilder)
+        {
+            CommonValidator.CheckForException(this.CaughtException);
+
+            memoryCacheTestBuilder(new MemoryCacheTestBuilder(this.TestContext));
+
+            return this.NewAndTestBuilder();
+        }
+    }
+}
