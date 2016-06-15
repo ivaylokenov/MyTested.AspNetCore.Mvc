a9ee54599a15d4ce87c3c80078b352cee220196f
apxoht <ivaylo.kenov@gmail.com>
2016-06-15 03:08:39 +0300

    Added tests for OptionsBuilder (#125)

--- src/MyTested.AspNetCore.Mvc.Options/Builders/Contracts/IOptionsBuilder.cs
+++ src/MyTested.AspNetCore.Mvc.Options/Builders/Contracts/IOptionsBuilder.cs
@@ -2,8 +2,16 @@
 {
     using System;
 
+    /// <summary>
+    /// Used for building configuration options.
+    /// </summary>
     public interface IOptionsBuilder
     {
+        /// <summary>
+        /// Sets initial values to the provided configuration options.
+        /// </summary>
+        /// <typeparam name="TOptions">Type of configuration options to set up.</typeparam>
+        /// <param name="optionsSetup">Action setting the configuration options.</param>
         void For<TOptions>(Action<TOptions> optionsSetup) where TOptions : class, new();
     }
 }
--- src/MyTested.AspNetCore.Mvc.Options/ControllerBuilderOptionsExtensions.cs
+++ src/MyTested.AspNetCore.Mvc.Options/ControllerBuilderOptionsExtensions.cs
@@ -5,8 +5,18 @@
     using Builders.Contracts;
     using Builders.Controllers;
 
+    /// <summary>
+    /// Contains configuration options extension methods for <see cref="IControllerBuilder{TController}"/>.
+    /// </summary>
     public static class ControllerBuilderOptionsExtensions
     {
+        /// <summary>
+        /// Sets initial values to the configuration options on the tested controller.
+        /// </summary>
+        /// <typeparam name="TController">Class representing ASP.NET Core MVC controller.</typeparam>
+        /// <param name="controllerBuilder">Instance of <see cref="IControllerBuilder{TController}"/> type.</param>
+        /// <param name="optionsBuilder">Action setting the configuration options by using <see cref="IOptionsBuilder"/>.</param>
+        /// <returns>The same <see cref="IControllerBuilder{TController}"/>.</returns>
         public static IControllerBuilder<TController> WithOptions<TController>(
             this IControllerBuilder<TController> controllerBuilder,
             Action<IOptionsBuilder> optionsBuilder)
