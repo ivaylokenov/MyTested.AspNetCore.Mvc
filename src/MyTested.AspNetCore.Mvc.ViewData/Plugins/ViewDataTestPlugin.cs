eac2723d6fe7766ab3ff853b239be92c3bed0779
apxoht <ivaylo.kenov@gmail.com>
2016-08-15 17:20:30 +0300

    Added MyTested.AspNetCore.Mvc.ViewData.Test (#125)

---
+++ src/MyTested.AspNetCore.Mvc.ViewData/Plugins/ViewDataTestPlugin.cs
@@ -0,0 +1,18 @@
+﻿namespace MyTested.AspNetCore.Mvc.Plugins
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class ViewDataTestPlugin : IDefaultRegistrationPlugin
+    {
+        public long Priority => -1000;
+
+        public Action<IServiceCollection> DefaultServiceRegistrationDelegate
+            => serviceCollection => serviceCollection
+                .AddMvcCore()
+                .AddFormatterMappings()
+                .AddViews()
+                .AddDataAnnotations()
+                .AddJsonFormatters();
+    }
+}
