3420360e2b0f5fdc535174bb6e070ae274677fa4
apxoht <ivaylo.kenov@gmail.com>
2017-02-25 16:57:17 +0200

    Added Travis CI (#269)

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore && dotnet build --configuration Release && dotnet test "samples/MusicStore/MusicStore.Test/project.json" --configuration Release && dotnet test "samples/ApplicationParts/ApplicationParts.Test/project.json" --configuration Release && dotnet test "samples/NoStartup/NoStartup.Test/project.json" --configuration Release && dotnet test "samples/Lite/Lite.Test/project.json" --configuration Release
