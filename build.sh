ec3ccae1fefc9e6f539aa71161658d7adc70eecd
apxoht <ivaylo.kenov@gmail.com>
2017-02-25 17:30:24 +0200

    Added Travis CI CLI version (#269)

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build --configuration Release && dotnet test "samples/MusicStore/MusicStore.Test/project.json" --configuration Release && dotnet test "samples/ApplicationParts/ApplicationParts.Test/project.json" --configuration Release && dotnet test "samples/NoStartup/NoStartup.Test/project.json" --configuration Release && dotnet test "samples/Lite/Lite.Test/project.json" --configuration Release
+dotnet restore
+dotnet build --configuration Release
+dotnet test "samples/MusicStore/MusicStore.Test/project.json" --configuration Release
+dotnet test "samples/ApplicationParts/ApplicationParts.Test/project.json" --configuration Release
+dotnet test "samples/NoStartup/NoStartup.Test/project.json" --configuration Release
+dotnet test "samples/Lite/Lite.Test/project.json" --configuration Release
