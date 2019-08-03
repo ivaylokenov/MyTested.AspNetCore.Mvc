dbf7f818960473a530310edb040cec8bb739729e
Bozhidar4 <bojidar.rangelov@gmail.com>
2019-08-03 22:07:00 +0300

    Added unit tests for covering MyTested.AspNetCore.Mvc.Authentication

---
+++ test/MyTested.AspNetCore.Mvc.Authentication.Test/BuildersTests/HttpRequestTests/HttpRequestBuilderTests.cs
@@ -0,0 +1,124 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.BuildersTests.HttpRequestTests
+{
+    using Microsoft.AspNetCore.Http;
+    using MyTested.AspNetCore.Mvc.Test.Setups.Controllers;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class HttpRequestBuilderTests
+    {
+        [Fact]
+        public void WithAuthenticatedUserShouldPopulateUserNameAndRolesFromListProperly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser("TestUserName", new List<string>
+                {
+                    "Administrator",
+                    "Moderator"
+                }))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+
+        [Fact]
+        public void WithAuthenticatedUserShouldPopulateUserNameAndRolesProperly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser("TestUserName", new string[]
+                {
+                    "Administrator",
+                    "Moderator"
+                }))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+
+        [Fact]
+        public void WithUserShouldPopulateUserIdentifierAndNameAndRolesProperly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser("TestIdentifier", "TestUserName", new List<string>
+                {
+                    "Administrator",
+                    "Moderator"
+                }))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+
+        [Fact]
+        public void WithAuthenticatedUserShouldPopulateUserIdentifierAndNameAndRolesProperly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser("TestIdentifier", "TestUserName", new string[]
+                {
+                    "Administrator",
+                    "Moderator"
+                }))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+
+        [Fact]
+        public void WithAuthenticatedUserShouldPopulateUserRolesProperly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser(new List<string>
+                {
+                    "Administrator",
+                    "Moderator"
+                }))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+
+        [Fact]
+        public void WithAuthenticatedUserShouldWorkCorrectly()
+        {
+            MyController<MvcController>
+                .Instance()
+                .WithHttpRequest(request => request
+                    .WithUser(user => user
+                    .WithIdentity(identity => identity
+                        .WithIdentifier("IdentityIdentifier")
+                        .WithUsername("IdentityUsername")
+                        .InRole("IdentityRole"))))
+                .ShouldPassForThe<HttpRequest>(builtRequest =>
+                {
+                    Assert.Equal(0, builtRequest.Query.Count);
+                    Assert.False(builtRequest.HasFormContentType);
+                    Assert.Equal(0, builtRequest.Cookies.Count);
+                });
+        }
+    }
+}
