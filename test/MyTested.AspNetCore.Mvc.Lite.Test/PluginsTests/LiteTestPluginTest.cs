2fcdae8adb21e3c4d66e4b909aceb580967a95a4
unknown <toni.stankov@uni.konstanz.de>
2019-08-18 20:12:29 +0300

    Added LiteTestPlugin tests

---
+++ test/MyTested.AspNetCore.Mvc.Lite.Test/PluginsTests/LiteTestPluginTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+    public class LiteTestPluginTest
+    {
+        [Fact]
+        public void ShouldHavePriorityWithDefaultValue()
+        {
+            var testPlugin = new LiteTestPlugin();
+
+            Assert.IsAssignableFrom<IDefaultRegistrationPlugin>(testPlugin);
+            Assert.NotNull(testPlugin);
+            Assert.Equal(0, testPlugin.Priority);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new LiteTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.DefaultServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new LiteTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.DefaultServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.DefaultServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 191);
+        }
+    }
+}
