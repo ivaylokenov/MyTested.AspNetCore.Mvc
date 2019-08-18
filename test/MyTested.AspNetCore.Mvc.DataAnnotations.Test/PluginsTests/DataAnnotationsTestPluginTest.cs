69f6f791a39408e2708510ccad853a09e151fa4a
unknown <toni.stankov@uni.konstanz.de>
2019-08-18 20:01:47 +0300

    Added DataAnnotationsTestPlugin tests

---
+++ test/MyTested.AspNetCore.Mvc.DataAnnotations.Test/PluginsTests/DataAnnotationsTestPluginTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class DataAnnotationsTestPluginTest
+    {
+        [Fact]
+        public void ShouldHavePriorityWithDefaultValue()
+        {
+            var testPlugin = new DataAnnotationsTestPlugin();
+
+            Assert.IsAssignableFrom<IDefaultRegistrationPlugin>(testPlugin);
+            Assert.NotNull(testPlugin);
+            Assert.Equal(-2000, testPlugin.Priority);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new DataAnnotationsTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.DefaultServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new DataAnnotationsTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.DefaultServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.DefaultServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 65);
+        }
+    }
+}
+
