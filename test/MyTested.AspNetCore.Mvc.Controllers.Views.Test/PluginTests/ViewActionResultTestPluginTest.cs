ced609016b03ed3fdc0764291fc820ae2ff0bd73
unknown <toni.stankov@uni.konstanz.de>
2019-08-18 20:31:51 +0300

    Added ViewActionResultsTestPlugin tests

---
+++ test/MyTested.AspNetCore.Mvc.Controllers.Views.Test/PluginTests/ViewActionResultTestPluginTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTested.AspNetCore.Mvc.Test.PluginTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class ViewActionResultTestPluginTest
+    {
+        [Fact]
+        public void ShouldHavePriorityWithDefaultValue()
+        {
+            var testPlugin = new ViewActionResultsTestPlugin();
+
+            Assert.IsAssignableFrom<IDefaultRegistrationPlugin>(testPlugin);
+            Assert.NotNull(testPlugin);
+            Assert.Equal(-1000, testPlugin.Priority);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new ViewActionResultsTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.DefaultServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new ViewActionResultsTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.DefaultServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.DefaultServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 126);
+        }
+    }
+}
