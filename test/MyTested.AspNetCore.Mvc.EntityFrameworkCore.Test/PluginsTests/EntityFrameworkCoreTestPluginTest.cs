f4e8d2c2e29dd4a86e2ddaaeb1ebee36da2d1b38
unknown <toni.stankov@uni.konstanz.de>
2019-08-31 18:40:49 +0300

    Added EntityFrameworkCoreTestPlugin tests (#183)

---
+++ test/MyTested.AspNetCore.Mvc.EntityFrameworkCore.Test/PluginsTests/EntityFrameworkCoreTestPluginTest.cs
@@ -0,0 +1,36 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Setups;
+    using Setups.Common;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class EntityFrameworkCoreTestPluginTest
+    {
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new EntityFrameworkCoreTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.ServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new EntityFrameworkCoreTestPlugin();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<CustomDbContext>(options => options.UseInMemoryDatabase(TestObjectFactory.TestDatabaseName));
+
+            testPlugin.ServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.ServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 13);
+        }
+    }
+}
