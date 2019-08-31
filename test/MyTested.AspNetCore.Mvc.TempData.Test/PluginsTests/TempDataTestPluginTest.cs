c26d842e72b2786f12b63c7eb0c213adee85b25b
unknown <toni.stankov@uni.konstanz.de>
2019-08-31 22:40:52 +0300

    Added TempDataTestPlugin tests (#183)

---
+++ test/MyTested.AspNetCore.Mvc.TempData.Test/PluginsTests/TempDataTestPluginTest.cs
@@ -0,0 +1,57 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class TempDataTestPluginTest
+    {
+        [Fact]
+        public void ShouldHavePriorityWithDefaultValue()
+        {
+            var testPlugin = new TempDataTestPlugin();
+
+            Assert.IsAssignableFrom<IDefaultRegistrationPlugin>(testPlugin);
+            Assert.NotNull(testPlugin);
+            Assert.Equal(-8000, testPlugin.Priority);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new TempDataTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.DefaultServiceRegistrationDelegate(null));
+            Assert.Throws<NullReferenceException>(() => testPlugin.ServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollectionForDefaultRegistration()
+        {
+            var testPlugin = new TempDataTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.DefaultServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.DefaultServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 63);
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new TempDataTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.ServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.ServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 1);
+        }
+    }
+}
