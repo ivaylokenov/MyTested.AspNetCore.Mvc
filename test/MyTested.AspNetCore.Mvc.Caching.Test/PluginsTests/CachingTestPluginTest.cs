9217f5bcacc969ef061fa997ffc3fc37f3b5e1a9
unknown <toni.stankov@uni.konstanz.de>
2019-08-31 23:16:41 +0300

    Added CachingTestPlugin tests (#183)

---
+++ test/MyTested.AspNetCore.Mvc.Caching.Test/PluginsTests/CachingTestPluginTest.cs
@@ -0,0 +1,32 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class CachingTestPluginTest
+    {
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new CachingTestPlugin();
+
+            Assert.Throws<NullReferenceException>(() => testPlugin.ServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new CachingTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.ServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.ServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 1);
+        }
+    }
+}
