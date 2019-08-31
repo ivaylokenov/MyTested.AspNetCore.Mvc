8eb80878dd522f0fc2096755429f2460b439ac61
unknown <toni.stankov@uni.konstanz.de>
2019-08-31 22:24:03 +0300

    Added RoutingTestPlugin tests (#183)

---
+++ test/MyTested.AspNetCore.Mvc.Routing.Test/PlungsTests/RoutingTestPluginTest.cs
@@ -0,0 +1,57 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.PlungsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class RoutingTestPluginTest
+    {
+        [Fact]
+        public void ShouldHavePriorityWithDefaultValue()
+        {
+            var testPlugin = new RoutingTestPlugin();
+
+            Assert.IsAssignableFrom<IDefaultRegistrationPlugin>(testPlugin);
+            Assert.NotNull(testPlugin);
+            Assert.Equal(-8000, testPlugin.Priority);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new RoutingTestPlugin();
+
+            Assert.Throws<ArgumentNullException>(() => testPlugin.DefaultServiceRegistrationDelegate(null));
+            Assert.Throws<NullReferenceException>(() => testPlugin.RoutingServiceRegistrationDelegate(null));
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollectionForDefaultRegistration()
+        {
+            var testPlugin = new RoutingTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.DefaultServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.DefaultServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 63);
+        }
+
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new RoutingTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.RoutingServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.RoutingServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 1);
+        }
+    }
+}
