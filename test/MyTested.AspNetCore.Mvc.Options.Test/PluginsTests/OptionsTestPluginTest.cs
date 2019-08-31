1d231d3d9991d1ed887e599573c63ee511a31db5
unknown <toni.stankov@uni.konstanz.de>
2019-08-31 21:04:05 +0300

    Added OptionsTestPlugin tests (#183)

---
+++ test/MyTested.AspNetCore.Mvc.Options.Test/PluginsTests/OptionsTestPluginTest.cs
@@ -0,0 +1,31 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.PluginsTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plugins;
+    using System;
+    using Xunit;
+
+    public class OptionsTestPluginTest
+    {
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithInvalidServiceCollection()
+        {
+            var testPlugin = new OptionsTestPlugin();
+
+            Assert.Throws<NullReferenceException>(() => testPlugin.ServiceRegistrationDelegate(null));
+        }
+        [Fact]
+        public void ShouldInvokeMethodOfTypeVoidWithValidServiceCollection()
+        {
+            var testPlugin = new OptionsTestPlugin();
+            var serviceCollection = new ServiceCollection();
+
+            testPlugin.ServiceRegistrationDelegate(serviceCollection);
+
+            var methodReturnType = testPlugin.ServiceRegistrationDelegate.Method.ReturnType.Name;
+
+            Assert.True(methodReturnType == "Void");
+            Assert.True(serviceCollection.Count == 1);
+        }
+    }
+}
