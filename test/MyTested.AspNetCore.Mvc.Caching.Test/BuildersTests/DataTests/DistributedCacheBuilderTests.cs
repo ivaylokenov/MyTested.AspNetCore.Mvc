9d2de806a86bc1a06d53adb757eae2996542f39d
HristoHentov <h_hentov@yahoo.com>
2019-09-14 19:47:52 +0300

    Initial test description (#247)

---
+++ test/MyTested.AspNetCore.Mvc.Caching.Test/BuildersTests/DataTests/DistributedCacheBuilderTests.cs
@@ -0,0 +1,62 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.BuildersTests.DataTests
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc.ApplicationParts;
+    using Microsoft.Extensions.Caching.Memory;
+    using Microsoft.Extensions.DependencyInjection;
+    using Setups;
+    using Setups.Controllers;
+    using Xunit;
+
+    public class DistributedCacheBuilderTests
+    {
+        public DistributedCacheBuilderTests()
+        {
+            MyApplication
+               .StartsFrom<DefaultStartup>()
+               .WithServices(services =>
+               {
+                   services.AddDistributedMemoryCache();
+
+                   services
+                       .AddMvc()
+                       .PartManager
+                       .ApplicationParts
+                       .Add(new AssemblyPart(this.GetType().Assembly));
+               });
+        }
+
+        [Fact]
+        public void InterfaceDescription()
+        {
+            MyController<MemoryCacheController>
+                .Instance()
+                .WithDistributedCache(distributedCache => distributedCache
+                    .WithEntry("Normal", "NormalValid")
+                    .WithEntry("Another", "AnotherValid"))
+                    .WithEntries(new Dictionary<object, object>
+                        {
+                            ["first"] = "firstValue",
+                            ["second"] = "secondValue",
+                            ["third"] = "thirdValue"
+                        })
+                    .WithEntries(new Dictionary<object, object>
+                    {
+                        ["fourth"] = "fourthValue",
+                        ["fifth"] = "fifthValue",
+                        ["sixth"] = "sixthValue"
+                    })
+                    .AndAlso()
+                    .WithEntries(new Dictionary<object, object>
+                    {
+                        ["seventh"] = "seventhValue",
+                        ["eight"] = "eightValue",
+                    })
+                .AndAlso()
+                .WithEntry("YetAnother", "YetAnotherValue")
+                .Calling(c => c.FullMemoryCacheAction(From.Services<IMemoryCache>()))
+                .ShouldReturn()
+                .Ok();
+        }
+    }
+}
