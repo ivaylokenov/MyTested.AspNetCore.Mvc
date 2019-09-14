79cc569c0ac8f5e4ac93b1577799daae4ff9465f
HristoHentov <h_hentov@yahoo.com>
2019-09-14 20:32:17 +0300

    Added initial interfaces (#247)

---
+++ src/MyTested.AspNetCore.Mvc.Caching/Builders/Contracts/Data/IDistributedCacheBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Contracts.Data
+{
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Caching.Distributed;
+
+    /// <summary>
+    /// Used for building mocked <see cref="IDistributedCache"/>.
+    /// </summary>
+    public interface IDistributedCacheBuilder
+    {
+        /// <summary>
+        /// Adds cache entry to the mocked <see cref="IDistributedCache"/>.
+        /// </summary>
+        /// <param name="key">Key of the cache entry.</param>
+        /// <param name="value">Value of the cache entry.</param>
+        /// <returns>The same <see cref="IAndDistributedCacheBuilder"/>.</returns>
+        IAndDistributedCacheBuilder WithEntry(string key, byte[] value);
+
+        /// <summary>
+        /// Adds cache entry to the mocked <see cref="IDistributedCache"/>.
+        /// </summary>
+        /// <param name="key">Key of the cache entry.</param>
+        /// <param name="value">Value of the cache entry.</param>
+        /// <param name="options"><see cref="DistributedCacheEntryOptions"/> of the cache entry.</param>
+        /// <returns>The same <see cref="IAndDistributedCacheBuilder"/>.</returns>
+        IAndDistributedCacheBuilder WithEntry(string key, byte[] value, DistributedCacheEntryOptions options);
+
+        /// <summary>
+        /// Adds cache entries to the mocked <see cref="IDistributedCache"/>.
+        /// </summary>
+        /// <param name="entries">Dictionary of cache entries.</param>
+        /// <returns>The same <see cref="IAndDistributedCacheBuilder"/>.</returns>
+        IAndDistributedCacheBuilder WithEntries(IDictionary<string, byte[]> entries);
+
+    }
+}
