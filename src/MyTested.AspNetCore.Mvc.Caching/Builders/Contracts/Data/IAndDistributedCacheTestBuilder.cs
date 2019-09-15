baffb84e86863447f906e926ec7d544df28925c2
HristoHentov <h_hentov@yahoo.com>
2019-09-15 12:28:27 +0300

    Added initial testing contracts (#247)

---
+++ src/MyTested.AspNetCore.Mvc.Caching/Builders/Contracts/Data/IAndDistributedCacheTestBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Contracts.Data
+{
+    /// <summary>
+    /// Used for adding AndAlso() method to the <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> tests.
+    /// </summary>
+    public interface IAndDistributedCacheTestBuilder : IDistributedCacheTestBuilder
+    {
+        /// <summary>
+        /// AndAlso method for better readability when testing <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/>
+        /// </summary>
+        /// <returns></returns>
+        IDistributedCacheTestBuilder AndAlso();
+    }
+}
