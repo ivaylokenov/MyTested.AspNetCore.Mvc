5490a78a60a90b2e6bbeef362928e6adaa02a562
HristoHentov <h_hentov@yahoo.com>
2019-09-15 11:34:15 +0300

    Added tests for ShouldHave().DistributedCache (#247)

---
+++ test/MyTested.AspNetCore.Mvc.Caching.Test/BuildersTests/DataTests/DistributedCacheTestBuilderTests.cs
@@ -0,0 +1,107 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test.BuildersTests.DataTests
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Caching.Distributed;
+    using Microsoft.Extensions.DependencyInjection;
+    using Setups;
+    using Setups.Controllers;
+    using Xunit;
+
+    public class DistributedCacheTestBuilderTests : IDisposable
+    {
+        public DistributedCacheTestBuilderTests()
+        {
+            MyApplication
+                .StartsFrom<DefaultStartup>()
+                .WithServices(services => services.AddDistributedMemoryCache());
+        }
+
+        [Fact]
+        public void ContainingEntryShouldNotThrowExceptionWithCorrectEntry()
+        {
+            var cacheValue = new byte[] { 127, 127, 127 };
+            MyController<MvcController>
+                .Instance()
+                .Calling(c => c.AddDistributedCacheAction())
+                .ShouldHave()
+                .DistriutedCache(cache => cache
+                    .ContainingEntry("Key", cacheValue))
+                .AndAlso()
+                .ShouldReturn()
+                .Ok();
+        }
+
+        [Fact]
+        public void ContainingEntryWithKeyShouldNotThrowExceptionWithCorrectEntry()
+        {
+            MyController<MvcController>
+                .Instance()
+                .Calling(c => c.AddDistributedCacheAction())
+                .ShouldHave()
+                .DistriutedCache(cache => cache
+                    .ContainingEntryWithKey("Key"))
+                .AndAlso()
+                .ShouldReturn()
+                .Ok();
+        }
+
+        [Fact]
+        public void ContainingEntryWithValueShouldNotThrowExceptionWithCorrectEntry()
+        {
+            var val = new byte[] { 127, 127, 127 };
+
+            MyController<MvcController>
+                .Instance()
+                .Calling(c => c.AddDistributedCacheAction())
+                .ShouldHave()
+                .DistriutedCache(cache => cache
+                    .ContainingEntryWithValue(val))
+                .AndAlso()
+                .ShouldReturn()
+                .Ok();
+        }
+
+        [Fact]
+        public void ContainingEntryWithOptionsShouldNotThrowExceptionWithCorrectEntry()
+        {
+            var cacheValue = new byte[] { 127, 127, 127 };
+            MyController<MvcController>
+                .Instance()
+                .Calling(c => c.AddDistributedCacheAction())
+                .ShouldHave()
+                .DistriutedCache(cache => cache
+                    .ContainingEntry("Key", cacheValue, new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpiration = new DateTimeOffset(new DateTime(2020, 1, 1, 1, 1, 1, DateTimeKind.Utc)),
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+                        SlidingExpiration = TimeSpan.FromMinutes(5)
+                    }))
+                .AndAlso()
+                .ShouldReturn()
+                .Ok();
+        }
+
+        [Fact]
+        public void ContainingEntriesShouldNotThrowExceptionWithCorrectEntries()
+        {
+            var cacheValue = new byte[] { 127, 127, 127 };
+            MyController<MvcController>
+                .Instance()
+                .Calling(c => c.AddDistributedCacheAction())
+                .ShouldHave()
+                .DistriutedCache(cache => cache
+                    .ContainingEntries(new Dictionary<string, byte[]>
+                    {
+                        {"FirstEntry", cacheValue },
+                        {"SecondEntry", cacheValue },
+                        {"ThirdEntry", cacheValue }
+                    }))
+                .AndAlso()
+                .ShouldReturn()
+                .Ok();
+        }
+
+        public void Dispose() => MyApplication.StartsFrom<DefaultStartup>();
+    }
+}
