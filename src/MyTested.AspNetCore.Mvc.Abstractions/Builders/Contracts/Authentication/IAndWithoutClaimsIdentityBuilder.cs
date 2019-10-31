32cd5dfd592e91abd131eebb1d03c8d07feb9b48
Borislav Vapsarov <b.vaptsarov@gmail.com>
2019-10-31 13:40:55 +0200

    #283 Adding Interfaces for without claims identity builder.

---
+++ src/MyTested.AspNetCore.Mvc.Abstractions/Builders/Contracts/Authentication/IAndWithoutClaimsIdentityBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Contracts.Authentication
+{
+    public interface IAndWithoutClaimsIdentityBuilder : IWithoutClaimsIdentityBuilder
+    {
+        IWithoutClaimsIdentityBuilder AndAlso();
+    }
+}
---
+++ src/MyTested.AspNetCore.Mvc.Abstractions/Builders/Contracts/Authentication/IWithoutClaimsIdentityBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Contracts.Authentication
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
+    public interface IWithoutClaimsIdentityBuilder
+    {
+        IAndWithoutClaimsIdentityBuilder WithoutNameType(string nameType);
+
+        IAndWithoutClaimsIdentityBuilder WithoutRoleType(string roleType);
+
+        IAndWithoutClaimsIdentityBuilder WithoutIdentifier(string identifier);
+
+        IAndWithoutClaimsIdentityBuilder WithoutUsername(string username);
+
+        IAndWithoutClaimsIdentityBuilder WithoutClaim(string type, string value);
+
+        IAndWithoutClaimsIdentityBuilder WithoutClaim(Claim claim);
+
+        IAndWithoutClaimsIdentityBuilder WithoutClaims(IEnumerable<Claim> claims);
+
+        IAndWithoutClaimsIdentityBuilder WithoutClaims(params Claim[] claims);
+    }
+}
