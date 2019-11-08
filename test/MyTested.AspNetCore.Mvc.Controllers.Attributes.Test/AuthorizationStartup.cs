801d68f7a1ddfbf9c660685144d8b85e6f81cc99
u.ashikov <u.ashikov@abv.bg>
2019-11-08 21:31:11 +0200

    Created custom startup with authorization policy.

---
+++ test/MyTested.AspNetCore.Mvc.Controllers.Attributes.Test/AuthorizationStartup.cs
@@ -0,0 +1,17 @@
+﻿namespace MyTested.AspNetCore.Mvc.Test
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Setups;
+
+    public class AuthorizationStartup : DefaultStartup
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+                    options.AddPolicy("Admin",
+                    policy => policy.RequireRole("Admin", "Moderator")));
+
+            base.ConfigureServices(services);
+        }
+    }
+}
