ca3b1dc5906a7c08fbd3e6517981df3d131bfc59
u.ashikov <u.ashikov@abv.bg>
2019-11-15 23:57:44 +0200

    Created attribute test builder for the ProducesResponseTypeAttribute (ivaylokenov#34).

---
+++ src/MyTested.AspNetCore.Mvc.Controllers.Attributes/Builders/Contracts/Attributes/IAndProducesResponseTypeAttributeTestBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Contracts.Attributes
+{
+    /// <summary>
+    /// Used for adding AndAlso() method to the <see cref="Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute" /> tests.
+    /// </summary>
+    public interface IAndProducesResponseTypeAttributeTestBuilder : IProducesResponseTypeAttributeTestBuilder
+    {
+        /// <summary>
+        /// AndAlso method for better readability when testing <see cref="Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute" />
+        /// </summary>
+        /// <returns>The same <see cref="IProducesResponseTypeAttributeTestBuilder"/>.</returns>
+        IProducesResponseTypeAttributeTestBuilder AndAlso();
+    }
+}
