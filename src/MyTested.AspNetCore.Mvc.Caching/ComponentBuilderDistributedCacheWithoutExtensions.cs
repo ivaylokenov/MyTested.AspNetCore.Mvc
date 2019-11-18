2211e5e9a34a8e7b1e34d362652b6671ed49b7fd
Borislav Vapsarov <b.vaptsarov@gmail.com>
2019-11-18 16:43:23 +0200

    #283 Adding comments for the component builder distributed cache.

--- src/MyTested.AspNetCore.Mvc.Caching/ComponentBuilderDistributedCacheWithoutExtensions.cs
+++ src/MyTested.AspNetCore.Mvc.Caching/ComponentBuilderDistributedCacheWithoutExtensions.cs
@@ -7,31 +7,68 @@
     using MyTested.AspNetCore.Mvc.Builders.Contracts.Data;
     using MyTested.AspNetCore.Mvc.Builders.Data.DistributedCache.WithoutDistributedCache;
 
+    /// <summary>
+    /// Contains <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> extension methods for <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/>.
+    /// </summary>
     public static class ComponentBuilderDistributedCacheWithoutExtensions
     {
+        /// <summary>
+        /// Clear all entities from <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> service.
+        /// </summary>
+        /// <typeparam name="TBuilder">Class representing ASP.NET Core MVC test builder.</typeparam>
+        /// <param name="builder">Instance of <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/> type.</param>
+        /// <returns>The same component builder.</returns>
         public static TBuilder WithoutDistributedCache<TBuilder>(
             this IBaseTestBuilderWithComponentBuilder<TBuilder> builder)
             where TBuilder : IBaseTestBuilder
             => builder.WithoutDistributedCache(cache => cache.WithoutAllEntries());
-  
+
+        /// <summary>
+        /// Remove given entity with key from <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> service.
+        /// </summary>
+        /// <typeparam name="TBuilder">Class representing ASP.NET Core MVC test builder.</typeparam>
+        /// <param name="builder">Instance of <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/> type.</param>
+        /// <param name="key">Key of the entity that will be removed.</param>
+        /// <returns>The same component builder.</returns>
         public static TBuilder WithoutDistributedCache<TBuilder>(
             this IBaseTestBuilderWithComponentBuilder<TBuilder> builder,
             string key)
             where TBuilder : IBaseTestBuilder
             => builder.WithoutDistributedCache(cache => cache.WithoutEntry(key));
-  
+
+        /// <summary>
+        /// Remove given entities from <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> service.
+        /// </summary>
+        /// <typeparam name="TBuilder">Class representing ASP.NET Core MVC test builder.</typeparam>
+        /// <param name="builder">Instance of <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/> type.</param>
+        /// <param name="keys">Keys of the entities that will be removed.</param>
+        /// <returns>The same component builder.</returns>
         public static TBuilder WithoutDistributedCache<TBuilder>(
             this IBaseTestBuilderWithComponentBuilder<TBuilder> builder,
             IEnumerable<string> keys)
             where TBuilder : IBaseTestBuilder
             => builder.WithoutDistributedCache(cache => cache.WithoutEntries(keys));
 
+        /// <summary>
+        /// Remove given entities from <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> service.
+        /// </summary>
+        /// <typeparam name="TBuilder">Class representing ASP.NET Core MVC test builder.</typeparam>
+        /// <param name="builder">Instance of <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/> type.</param>
+        /// <param name="keys">Keys of the entities that will be removed.</param>
+        /// <returns>The same component builder.</returns>
         public static TBuilder WithoutDistributedCache<TBuilder>(
             this IBaseTestBuilderWithComponentBuilder<TBuilder> builder,
             params string[] keys)
             where TBuilder : IBaseTestBuilder
             => builder.WithoutDistributedCache(cache => cache.WithoutEntries(keys));
 
+        /// <summary>
+        /// Remove entity or entities from <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> service.
+        /// </summary>
+        /// <typeparam name="TBuilder">Class representing ASP.NET Core MVC test builder.</typeparam>
+        /// <param name="builder">Instance of <see cref="IBaseTestBuilderWithComponentBuilder{TBuilder}"/> type.</param>
+        /// <param name="distributedCacheBuilder">Action setting the <see cref="Microsoft.Extensions.Caching.Distributed.IDistributedCache"/> values by using <see cref="IWithoutDistributedCacheBuilder"/>.</param>
+        /// <returns>The same component builder.</returns>
         public static TBuilder WithoutDistributedCache<TBuilder>(
             this IBaseTestBuilderWithComponentBuilder<TBuilder> builder,
             Action<IWithoutDistributedCacheBuilder> distributedCacheBuilder)
