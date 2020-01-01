b44a6b4f26fb11b3b0ce01ba9d335526ec9e0ff6
ivaylokenov <ivaylo.kenov@gmail.com>
2020-01-01 13:09:41 +0200

    Added implementations for the IResultDetailsTestBuilder interfaces (#359)

---
+++ src/MyTested.AspNetCore.Mvc.Models/Builders/Results/ResultDetailsTestBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace MyTested.AspNetCore.Mvc.Builders.Results
+{
+    using Base;
+    using Contracts.Models;
+    using Contracts.Results;
+    using Internal.TestContexts;
+
+    /// <summary>
+    /// Used for testing the result members.
+    /// </summary>
+    public class ResultDetailsTestBuilder 
+        : BaseTestBuilderWithActionContext, 
+        IResultDetailsTestBuilder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResultDetailsTestBuilder"/> class.
+        /// </summary>
+        /// <param name="testContext"><see cref="ActionTestContext"/> containing data about the currently executed assertion chain.</param>
+        public ResultDetailsTestBuilder(ActionTestContext testContext)
+            : base(testContext)
+        {
+        }
+
+        /// <inheritdoc />
+        public IAndModelDetailsTestBuilder<TResult> EqualTo<TResult>(TResult result)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
