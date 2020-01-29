defd09c5ebf3f4b208e1f8bf0cd4c79b8dbf3e39
Yulian Ashikov <u.ashikov@abv.bg>
2020-01-29 22:50:12 +0200

    Added AuthorizeAttribute to the ApiController (#41). Added the ProducesResponseType attribute to fix broken unit tests (#34).

--- test/MyTested.AspNetCore.Mvc.Test.Setups/Controllers/ApiController.cs
+++ test/MyTested.AspNetCore.Mvc.Test.Setups/Controllers/ApiController.cs
@@ -4,10 +4,13 @@
     using ActionFilters;
     using Models;
     using Pipeline;
+    using Microsoft.AspNetCore.Authorization;
 
+    [Authorize]
     [ApiController]
     [Consumes("application/json", "application/xml")]
     [Produces("application/json", "application/xml", Type = typeof(ResponseModel), Order = 1)]
+    [ProducesResponseType(200, Type = typeof(ResponseModel))]
     [MiddlewareFilter(typeof(Pipeline), Order = 2)]
     [ServiceFilter(typeof(CustomActionFilter), Order = 2)]
     [TypeFilter(typeof(CustomActionFilterWithArgs), Order = 2, Arguments = new object[] { 10 })]
