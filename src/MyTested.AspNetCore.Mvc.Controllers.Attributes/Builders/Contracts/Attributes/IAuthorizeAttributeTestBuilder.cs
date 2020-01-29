6d62a2bd0cacb3490b813b607a04abbf11c4787f
Yulian Ashikov <u.ashikov@abv.bg>
2020-01-29 20:14:49 +0200

    Reverted wrong commited code (#41).

--- src/MyTested.AspNetCore.Mvc.Controllers.Attributes/Builders/Contracts/Attributes/IAuthorizeAttributeTestBuilder.cs
+++ src/MyTested.AspNetCore.Mvc.Controllers.Attributes/Builders/Contracts/Attributes/IAuthorizeAttributeTestBuilder.cs
@@ -17,30 +17,6 @@
 
         /// <summary>
         /// Tests whether a <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute"/>
-        /// contains the provided role in its <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute.Roles"/> values.
-        /// </summary>
-        /// <param name="role">Expected role.</param>
-        /// <returns>The same <see cref="IAndAuthorizeAttributeTestBuilder"/>.</returns>
-        IAndAuthorizeAttributeTestBuilder WithRole(string role);
-
-        /// <summary>
-        /// Tests whether a <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute"/>
-        /// contains the provided roles in its <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute.Roles"/> values.
-        /// </summary>
-        /// <param name="roles">Expected roles.</param>
-        /// <returns>The same <see cref="IAndAuthorizeAttributeTestBuilder"/>.</returns>
-        IAndAuthorizeAttributeTestBuilder WithRoles(IEnumerable<string> roles);
-
-        /// <summary>
-        /// Tests whether a <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute"/>
-        /// contains the provided roles in its <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute.Roles"/> values.
-        /// </summary>
-        /// <param name="roles">Expected roles.</param>
-        /// <returns>The same <see cref="IAndAuthorizeAttributeTestBuilder"/>.</returns>
-        IAndAuthorizeAttributeTestBuilder WithRoles(params string[] roles);
-
-        /// <summary>
-        /// Tests whether a <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute"/>
         /// has the same <see cref="Microsoft.AspNetCore.Authorization.AuthorizeAttribute.AuthenticationSchemes"/> value as the provided one.
         /// </summary>
         /// <param name="authenticationSchemes">Expected authentication schemes.</param>
